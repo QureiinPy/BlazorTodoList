dcdc6d78b6943c6a9b2d08a0b880448629e2dc3a
Dmitry Polezaev <dimapolezaev6283@gmail.com>
2024-01-29 16:13:29 +0500

    v.0.9

---
+++ DB/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlazorTodo.Model;
+
+namespace BlazorTodo.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Model.Task> Tasks { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public AppDbContext()
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=postgres;Username=postgres;Password=postqwerty");
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Model.Task>(builder =>
+        //    {
+        //        builder.ToTable("Tasks");
+
+        //        builder.Property(task => task.TaskId)
+        //        .HasColumnName("TaskId")
+        //        .UseSerialColumn()
+        //        .UseIdentityColumn()
+        //        .UseSequence();
+        //    });
+        //}
+    }
+}
---
+++ Model/Task.cs
@@ -0,0 +1,18 @@
+﻿
+namespace BlazorTodo.Model;
+
+public class Task
+{
+    public int TaskId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsDone { get; set; } = false;
+    public string CreatedOn { get; set; } = DateTime.Now.ToString("dd.MM.yyyy HH:mm");
+    public string UpdatedOn { get; set;} = DateTime.Now.ToString("dd.MM.yyyy HH:mm");
+
+    public override string ToString()
+    {
+        return $"{TaskId}) {Title} {IsDone.ToString().ToUpper()}";
+    }
+
+}
